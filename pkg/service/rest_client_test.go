42be43b1f72e277c7e0329157b1b4129e1968bf1
chai2010 <chaishushan@gmail.com>
2022-06-24 13:49:20 +0800

    test: pkg/service add no method test for CallRestMethod

--- pkg/service/rest_client_test.go
+++ pkg/service/rest_client_test.go
@@ -22,3 +22,16 @@ func TestCallRestMethod_ping(t *testing.T) {
 		t.Fatalf("expect %q, got %q", args.Value, result.Value)
 	}
 }
+
+func TestCallRestMethod_noMethod(t *testing.T) {
+	var args = gpyrpc.Ping_Args{Value: "ping"}
+	var result gpyrpc.Ping_Result
+
+	var err = CallRestMethod(
+		"http://"+tRestServerAddr, "UnknownService.noMethod",
+		&args, &result,
+	)
+	if err == nil {
+		t.Fatalf("expect error, got %v", err)
+	}
+}
