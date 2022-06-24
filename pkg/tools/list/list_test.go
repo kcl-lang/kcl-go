d41da814e4a1849e6cffde6cf5c847bc098e1158
chai2010 <chaishushan@gmail.com>
2022-06-24 12:29:07 +0800

    test: pkg/tools/list add restful unit test

--- pkg/tools/list/list_test.go
+++ pkg/tools/list/list_test.go
@@ -7,6 +7,9 @@ import (
 	"sort"
 	"strings"
 	"testing"
+
+	"kusionstack.io/kclvm-go/pkg/service"
+	"kusionstack.io/kclvm-go/pkg/spec/gpyrpc"
 )
 
 func TestListDepFiles(t *testing.T) {
@@ -30,8 +33,65 @@ func TestListDepFiles(t *testing.T) {
 	}
 }
 
+func TestListDepFiles_restful(t *testing.T) {
+	var args = gpyrpc.ListDepFiles_Args{
+		WorkDir:       "../../../testdata/app0",
+		UseAbsPath:    false,
+		IncludeAll:    false,
+		UseFastParser: true,
+	}
+	var result gpyrpc.ListDepFiles_Result
+
+	var err = service.CallRestMethod(
+		"http://"+tRestServerAddr, "KclvmService.ListDepFiles",
+		&args, &result,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	files := result.Files
+
+	expect := []string{
+		"main.k",
+		"app0/before/base.k",
+		"app0/main.k",
+		"app0/sub/sub.k",
+	}
+
+	sort.Strings(files)
+	sort.Strings(expect)
+
+	if !reflect.DeepEqual(files, expect) {
+		t.Fatalf("\nexpect = %v\ngot    = %v", expect, files)
+	}
+}
+
 func TestListDepFiles_failed(t *testing.T) {
 	_, err := ListDepFiles("../../../testdata/app0-failed", nil)
+	if err == nil {
+		t.Fatal("expect error, got nil")
+	}
+
+	expectErrMsg := "package app0-failed/sub_not_found: no kcl file"
+	if !strings.Contains(err.Error(), expectErrMsg) {
+		t.Fatalf("expect %q, got %q", expectErrMsg, err)
+	}
+}
+
+func TestListDepFiles_restfulFailed(t *testing.T) {
+	var args = gpyrpc.ListDepFiles_Args{
+		WorkDir:       "../../../testdata/app0-failed",
+		UseAbsPath:    false,
+		IncludeAll:    false,
+		UseFastParser: true,
+	}
+	var result gpyrpc.ListDepFiles_Result
+
+	var err = service.CallRestMethod(
+		"http://"+tRestServerAddr, "KclvmService.ListDepFiles",
+		&args, &result,
+	)
 	if err == nil {
 		t.Fatal("expect error, got nil")
 	}
