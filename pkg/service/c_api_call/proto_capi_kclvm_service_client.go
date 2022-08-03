b3ca01516907817ae670984b221477d31a32882d
never <chenxiangfei.cxf@antgroup.com>
2022-08-03 18:47:20 +0800

    temp

--- pkg/service/c_api_call/proto_capi_kclvm_service_client.go
+++ pkg/service/c_api_call/proto_capi_kclvm_service_client.go
@@ -1,7 +1,7 @@
 package capicall
 
 // #cgo CFLAGS: -I${SRCDIR}/packaged/include
-// #cgo LDFLAGS: -lkclvm_cli_cdylib
+// #cgo LDFLAGS: -lkclvm_api
 // #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-amd64 -L${SRCDIR}/packaged/lib/linux-amd64
 // #cgo linux,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-aarch64 -L${SRCDIR}/packaged/lib/linux-aarch64
 // #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/darwin-amd64 -L${SRCDIR}/packaged/lib/darwin-amd64
