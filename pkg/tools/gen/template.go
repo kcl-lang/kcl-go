5a65fd92cf54cd1247bfcc2be51d76d1e7907266
jakezhu9 <jakezhu9@gmail.com>
2023-08-07 11:52:54 +0800

    feat: generate documentation comments from json schema

--- pkg/tools/gen/template.go
+++ pkg/tools/gen/template.go
@@ -4,10 +4,13 @@ import (
 	_ "embed"
 	"fmt"
 	"io"
+	"strings"
 	"text/template"
 )
 
 var (
+	//go:embed templates/document.gotmpl
+	documentTmpl string
 	//go:embed templates/header.gotmpl
 	headerTmpl string
 	//go:embed templates/validator.gotmpl
@@ -20,10 +23,12 @@ var funcs = template.FuncMap{
 	"formatType":  formatType,
 	"formatValue": formatValue,
 	"formatName":  formatName,
+	"formatDoc":   formatDoc,
 }
 
 func (k *kclGenerator) genKclSchema(w io.Writer, s kclSchema) error {
 	tmpl := &template.Template{}
+	tmpl = addTemplate(tmpl, "document", documentTmpl)
 	tmpl = addTemplate(tmpl, "header", headerTmpl)
 	tmpl = addTemplate(tmpl, "validator", validatorTmpl)
 	tmpl = addTemplate(tmpl, "schema", schemaTmpl)
@@ -58,4 +63,9 @@ func formatValue(v interface{}) string {
 
 func formatName(name string) string {
 	return name
+}
+
+func formatDoc(doc, indent string) string {
+	doc = strings.Replace(doc, "\r\n", "\n", -1)
+	return indent + strings.Replace(doc, "\n", "\n"+indent, -1)
 }
