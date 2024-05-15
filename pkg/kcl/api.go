919e81d845b44a201851f99d38045650f420631a
xiaok29 <1526783667@qq.com>
2024-05-15 17:23:08 +0800

    🎈 perf: 减少嵌套,优先退出
    Signed-off-by: xiaok29 <1526783667@qq.com>

--- pkg/kcl/api.go
+++ pkg/kcl/api.go
@@ -491,26 +491,30 @@ func run(pathList []string, opts ...Option) (*KCLResultList, error) {
 // but will return an error if anything else is on the line.
 func splitDocuments(s string) ([]string, error) {
 	docs := make([]string, 0)
-	if len(s) > 0 {
-		// The YAML document separator is any line that starts with ---
-		yamlSeparatorRegexp := regexp.MustCompile(`\n---.*\n`)
-
-		// Find all separators, check them for invalid content, and append each document to docs
-		separatorLocations := yamlSeparatorRegexp.FindAllStringIndex(s, -1)
-		prev := 0
-		for i := range separatorLocations {
-			loc := separatorLocations[i]
-			separator := s[loc[0]:loc[1]]
-			// If the next non-whitespace character on the line following the separator is not a comment, return an error
-			trimmedContentAfterSeparator := strings.TrimSpace(separator[4:])
-			if len(trimmedContentAfterSeparator) > 0 && trimmedContentAfterSeparator[0] != '#' {
-				return nil, fmt.Errorf("invalid document separator: %s", strings.TrimSpace(separator))
-			}
-
-			docs = append(docs, s[prev:loc[0]])
-			prev = loc[1]
-		}
-		docs = append(docs, s[prev:])
+	if len(s) == 0 {
+		return docs, nil
 	}
+
+	// The YAML document separator is any line that starts with ---
+	yamlSeparatorRegexp := regexp.MustCompile(`\n---.*\n`)
+
+	// Find all separators, check them for invalid content, and append each document to docs
+	separatorLocations := yamlSeparatorRegexp.FindAllStringIndex(s, -1)
+	prev := 0
+	for i := range separatorLocations {
+		loc := separatorLocations[i]
+		separator := s[loc[0]:loc[1]]
+		// If the next non-whitespace character on the line following the separator is not a comment, return an error
+		trimmedContentAfterSeparator := strings.TrimSpace(separator[4:])
+		if len(trimmedContentAfterSeparator) > 0 && trimmedContentAfterSeparator[0] != '#' {
+			return nil, fmt.Errorf("invalid document separator: %s", strings.TrimSpace(separator))
+		}
+
+		docs = append(docs, s[prev:loc[0]])
+		prev = loc[1]
+	}
+
+	docs = append(docs, s[prev:])
+
 	return docs, nil
 }
