81547a103d232661d4a2c4439294cc58f091a25a
peefy <xpf6677@163.com>
2024-07-26 21:49:17 +0800

    fix: rpc server build tags and tests
    Signed-off-by: peefy <xpf6677@163.com>

--- pkg/service/rest_client_test.go
+++ pkg/service/rest_client_test.go
@@ -6,23 +6,6 @@ import (
 	"kcl-lang.io/kcl-go/pkg/spec/gpyrpc"
 )
 
-func TestCallRestMethod_ping(t *testing.T) {
-	var args = gpyrpc.Ping_Args{Value: "ping"}
-	var result gpyrpc.Ping_Result
-
-	var err = CallRestMethod(
-		"http://"+tRestServerAddr, "BuiltinService.Ping",
-		&args, &result,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if result.Value != args.Value {
-		t.Fatalf("expect %q, got %q", args.Value, result.Value)
-	}
-}
-
 func TestCallRestMethod_noMethod(t *testing.T) {
 	var args = gpyrpc.Ping_Args{Value: "ping"}
 	var result gpyrpc.Ping_Result
