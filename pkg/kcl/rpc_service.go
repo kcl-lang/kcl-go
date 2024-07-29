1a107263f28fc192c038e5f521b532b4b7498c0e
peefy <xpf6677@163.com>
2024-07-29 12:58:00 +0800

    chore: bump native go build tags to rpc
    Signed-off-by: peefy <xpf6677@163.com>

--- pkg/kcl/rpc_service.go
+++ pkg/kcl/rpc_service.go
@@ -6,8 +6,8 @@ package kcl
 import "kcl-lang.io/kcl-go/pkg/service"
 
 // Service returns the interaction interface between KCL Go SDK and KCL Rust core.
-// When `go build tags=native` is opened, use CGO and dynamic libraries to interact.
-// When closed, use the default RPC interaction logic to avoid CGO usage.
+// When `go build tags=rpc` is opened, use the default RPC interaction logic to avoid CGO usage.
+// When closed, use CGO and dynamic libraries to interact.
 func Service() service.KclvmService {
 	return service.NewKclvmServiceClient()
 }
--- pkg/kcl/service.go
+++ pkg/kcl/service.go
@@ -9,8 +9,8 @@ import (
 )
 
 // Service returns the interaction interface between KCL Go SDK and KCL Rust core.
-// When `go build tags=native` is opened, use CGO and dynamic libraries to interact.
-// When closed, use the default RPC interaction logic to avoid CGO usage.
+// When `go build tags=rpc` is opened, use the default RPC interaction logic to avoid CGO usage.
+// When closed, use CGO and dynamic libraries to interact.
 func Service() service.KclvmService {
 	return native.NewNativeServiceClient()
 }
